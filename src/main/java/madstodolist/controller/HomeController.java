97601e42c4d98e34cec36fad124bb101336e6005
Francisco José Ramírez Paraíso <fjrp9@alu.ua.es>
2023-09-30 11:31:09 +0200

    Ya diferencia en el about si el usuario está registrado o no y funcionando los botoes de ToDoList y Tareas

--- src/main/java/madstodolist/controller/HomeController.java
+++ src/main/java/madstodolist/controller/HomeController.java
@@ -1,5 +1,9 @@
 package madstodolist.controller;
 
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.dto.UsuarioData;
+import madstodolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    ManagerUserSession managerUserSession;
+
+    @Autowired
+    UsuarioService usuarioService;
+
     @GetMapping("/about")
     public String about(Model model) {
+
+        if(managerUserSession.usuarioLogeado() == null){
+            model.addAttribute("logeado",false);
+            model.addAttribute("usuario", null);
+        }else{
+            model.addAttribute("logeado", true);
+            UsuarioData usuario = usuarioService.findById(managerUserSession.usuarioLogeado());
+            model.addAttribute("usuario", usuario);
+        }
+
         return "about";
     }
 
