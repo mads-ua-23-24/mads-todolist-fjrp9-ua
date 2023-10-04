232d07314d0826ad033c8518cb40ca3746916950
Francisco José Ramírez Paraíso <fjrp9@alu.ua.es>
2023-10-04 12:39:19 +0200

    Añadida vista de descripcion, accesible desde el enlace de la vista del listado y añadido método en el controlador

--- src/main/java/madstodolist/controller/UsuarioController.java
+++ src/main/java/madstodolist/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSession;
+import madstodolist.dto.TareaData;
 import madstodolist.dto.UsuarioData;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -33,5 +36,21 @@ public class UsuarioController {
             model.addAttribute("usuario", usuario);
         }
         return "listaUsuarios";
+    }
+
+    @GetMapping("/registrados/{id}")
+    public String descripcionUsuario(@PathVariable(value="id") Long idUsuario, Model model, HttpSession session){
+
+        //Todavía queda terminarlo
+
+        if(managerUserSession.usuarioLogeado() == null){
+            model.addAttribute("logeado",false);
+            model.addAttribute("usuario", null);
+        }else{
+            model.addAttribute("logeado", true);
+            UsuarioData usuario = usuarioService.findById(managerUserSession.usuarioLogeado());
+            model.addAttribute("usuario", usuario);
+        }
+        return "descripcionUsuario";
     }
 }
