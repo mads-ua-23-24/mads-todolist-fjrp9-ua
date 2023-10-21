82fee397658eddfba4737ebc16b45117496312d4
Francisco José Ramírez Paraíso <fjrp9@alu.ua.es>
2023-10-21 14:30:06 +0200

    Primer test - Clase Equipo

---
+++ src/main/java/madstodolist/model/Equipo.java
@@ -0,0 +1,34 @@
+package madstodolist.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+public class Equipo {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String nombre;
+
+    public Equipo() {}
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() { return nombre; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+}
