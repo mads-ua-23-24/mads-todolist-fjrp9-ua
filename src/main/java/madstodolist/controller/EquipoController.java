442a0ded25d6f892a5930460439d684a11d9f4b8
Francisco José Ramírez Paraíso <fjrp9@alu.ua.es>
2023-10-23 18:34:05 +0200

    Añadidos control de privilegios y logeo en la vista y en el controlador para listar equipos

--- src/main/java/madstodolist/controller/EquipoController.java
+++ src/main/java/madstodolist/controller/EquipoController.java
@@ -2,6 +2,7 @@ package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSession;
 import madstodolist.controller.exception.UsuarioNoAdministradorException;
+import madstodolist.controller.exception.UsuarioNoLogeadoException;
 import madstodolist.dto.EquipoData;
 import madstodolist.dto.UsuarioData;
 import madstodolist.service.UsuarioService;
@@ -25,14 +26,25 @@ public class EquipoController {
     @Autowired
     ManagerUserSession managerUserSession;
 
+    private void comprobarUsuarioLogeado(Long idUsuario) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (!idUsuario.equals(idUsuarioLogeado))
+            throw new UsuarioNoLogeadoException();
+    }
+
     @GetMapping("/equipos")
     public String listarEquipos(Model model){
+
+        Long IdUsuarioLogeado = managerUserSession.usuarioLogeado();
+        comprobarUsuarioLogeado(IdUsuarioLogeado);
 
         List<EquipoData> equipos = equipoService.findAllOrdenadoPorNombre();
         model.addAttribute("equipos", equipos);
         model.addAttribute("logeado", true);
         UsuarioData usuario = usuarioService.findById(managerUserSession.usuarioLogeado());
         model.addAttribute("usuarioPrincipal", usuario);
+        boolean administrador = usuarioService.esAdmin(IdUsuarioLogeado);
+        model.addAttribute("administrador", administrador);
 
         return "listaEquipos";
     }
