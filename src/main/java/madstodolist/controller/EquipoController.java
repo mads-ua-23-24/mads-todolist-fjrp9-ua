a29509cfaea5a8770c6512581d3ba22f162aecee
Francisco José Ramírez Paraíso <fjrp9@alu.ua.es>
2023-10-24 10:02:32 +0200

    Añadida vista para listar los usuarios de un equipo, metodo en el controlador y añadido boton para acceder en listaEquipos.html

--- src/main/java/madstodolist/controller/EquipoController.java
+++ src/main/java/madstodolist/controller/EquipoController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import madstodolist.service.EquipoService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -47,5 +48,24 @@ public class EquipoController {
         model.addAttribute("administrador", administrador);
 
         return "listaEquipos";
+    }
+
+    @GetMapping("/equipos/{id}/usuarios")
+    public String listaUsuariosEquipo(Model model, @PathVariable(value="id") Long idEquipo){
+
+            Long IdUsuarioLogeado = managerUserSession.usuarioLogeado();
+            comprobarUsuarioLogeado(IdUsuarioLogeado);
+
+            List<UsuarioData> usuarios = equipoService.usuariosEquipo(idEquipo);
+            model.addAttribute("usuarios", usuarios);
+            model.addAttribute("logeado", true);
+            UsuarioData usuario = usuarioService.findById(managerUserSession.usuarioLogeado());
+            model.addAttribute("usuarioPrincipal", usuario);
+            boolean administrador = usuarioService.esAdmin(IdUsuarioLogeado);
+            model.addAttribute("administrador", administrador);
+            EquipoData equipoData = equipoService.recuperarEquipo(idEquipo);
+            model.addAttribute("equipo", equipoData);
+
+            return "listaUsuariosEquipo";
     }
 }
